619864e6fec5f5aae33e1f8bc48e60afe84f436b
kulraj chavda <kulraj710@gmail.com>
2024-08-01 19:35:18 +0530

    [feat] : calc RSI

--- main.py
+++ main.py
@@ -12,4 +12,29 @@ end = dt.datetime.now()
 
 data = yf.download(ticker,start=start, end=end)
 
-print(data)
+delta = data['Adj Close'].diff(1)
+delta.dropna(inplace=True)
+
+positive = delta.copy()
+negative = delta.copy()
+
+# scrap negavite values
+positive[ positive < 0] = 0
+
+# scrap positive values
+negative[ negative > 0] = 0
+
+days = 14
+
+avgerate_gain = positive.rolling(window=days).mean()
+avgerate_loss = negative.rolling(windows=days).mean()
+
+# calc relative strength 
+relative_strength = avgerate_gain / avgerate_loss
+RSI = 100.0 - (100.0 / (1.0 + relative_strength))
+
+combined = pd.DataFrame()
+combined['Adj Close'] = data['Adj Close']
+combined['RSI'] = RSI
+
+
